74544309d96c7b9cc7f563bcb6c5f4c136276a6c
Maxim <MaximKomoza@yandex.ru>
2023-04-13 16:39:03 +0700

    edit UI

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       text: "I need to do something",
       done: true,
       isExpanded: false,
+      isEditing: false,
     },
     {
       id: 1,
@@ -17,6 +18,7 @@ function App() {
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe excepturi officiis eos magni dicta! Ullam repellendus at culpa ex, rerum ea perferendis porro velit magni temporibus nam quae, consectetur facilis blanditiis eos possimus veniam alias sapiente doloremque ipsa. Minima nostrum accusamus quisquam minus natus corrupti doloribus amet pariatur veritatis facere.",
       done: false,
       isExpanded: false,
+      isEditing: false,
     },
     {
       id: 2,
@@ -24,6 +26,7 @@ function App() {
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe excepturi officiis eos magni dicta! Ullam repellendus at culpa ex, rerum ea perferendis porro velit magni temporibus nam quae, consectetur facilis blanditiis eos possimus veniam alias sapiente doloremque ipsa. Minima nostrum accusamus quisquam minus natus corrupti doloribus amet pariatur veritatis facere.",
       done: true,
       isExpanded: false,
+      isEditing: false,
     },
     {
       id: 3,
@@ -31,6 +34,7 @@ function App() {
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe excepturi officiis eos magni dicta! Ullam repellendus at culpa ex, rerum ea perferendis porro velit magni temporibus nam quae, consectetur facilis blanditiis eos possimus veniam alias sapiente doloremque ipsa. Minima nostrum accusamus quisquam minus natus corrupti doloribus amet pariatur veritatis facere.",
       done: false,
       isExpanded: false,
+      isEditing: false,
     },
     {
       id: 4,
@@ -38,6 +42,7 @@ function App() {
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe excepturi officiis eos magni dicta! Ullam repellendus at culpa ex, rerum ea perferendis porro velit magni temporibus nam quae, consectetur facilis blanditiis eos possimus veniam alias sapiente doloremque ipsa. Minima nostrum accusamus quisquam minus natus corrupti doloribus amet pariatur veritatis facere.",
       done: false,
       isExpanded: false,
+      isEditing: false,
     },
     {
       id: 5,
@@ -45,6 +50,7 @@ function App() {
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe excepturi officiis eos magni dicta! Ullam repellendus at culpa ex, rerum ea perferendis porro velit magni temporibus nam quae, consectetur facilis blanditiis eos possimus veniam alias sapiente doloremque ipsa. Minima nostrum accusamus quisquam minus natus corrupti doloribus amet pariatur veritatis facere.",
       done: true,
       isExpanded: false,
+      isEditing: false,
     },
   ]);
   const renderTasks = () => {
@@ -56,11 +62,49 @@ function App() {
           className="task"
           onClick={() => handleTaskClick(task.id)}
         >
-          {task.name}
-          {task.isExpanded && <div className="task-text">{task.text}</div>}
+          <div
+            className={`inputEdit task-name ${task.isEditing ? "--edit" : ""}`}
+            id={`task-name-${task.index}`}
+            type="text"
+            contentEditable={task.isEditing}
+            onClick={(event) => handleTextClick(event, task.id)}
+          >
+            {task.name}
+          </div>
+          {task.isExpanded && (
+            <div
+              id={`task-text-${task.index}`}
+              className={`inputEdit task-text ${
+                task.isEditing ? "--edit" : ""
+              }`}
+              type="text"
+              contentEditable={task.isEditing}
+              onClick={(event) => handleTextClick(event, task.id)}
+            >
+              {task.text}
+            </div>
+          )}
         </li>
-        <div className="edit"></div>
-        <div className="trash" onClick={() => handleTrashClick(task.id)}></div>
+        {!task.isEditing && (
+          <div className="button-box">
+            <div
+              className="edit"
+              onClick={() => handleEditClick(task.id)}
+            ></div>
+            <div
+              className="trash"
+              onClick={() => handleTrashClick(task.id)}
+            ></div>
+          </div>
+        )}
+        {task.isEditing && (
+          <div className="button-box">
+            <div className='task-button -save --active'
+            >+</div>
+            <div className='task-button -close --active'
+            >-</div>
+          </div>
+        )}
       </div>
     ));
   };
@@ -72,6 +116,15 @@ function App() {
   const [taskText, setTaskText] = useState("");
   const [taskName, setTaskName] = useState("");
 
+  const handleEditClick = (index) => {
+    if (!tasks[index].isExpanded) {
+      tasks[index].isExpanded = true;
+    }
+
+    tasks[index].isEditing = !tasks[index].isEditing;
+    setTasks([...tasks]);
+  };
+
   const handleTrashClick = (index) => {
     const taskBox = document.getElementById(`task-box-${index}`);
     taskBox.classList.add("deleted");
@@ -79,9 +132,15 @@ function App() {
     const newTasks = [...tasks];
     newTasks.splice(index, 1);
     setTimeout(() => {
-      document.querySelector('.deleted').classList.remove('deleted');
+      document.querySelector(".deleted").classList.remove("deleted");
       setTasks(newTasks.map((task, i) => ({ ...task, id: i })));
     }, 600);
+  };
+
+  const handleTextClick = (event, id) => {
+    if (tasks[id].isEditing) {
+      event.stopPropagation();
+    }
   };
 
   const handleAddTask = () => {
@@ -93,7 +152,8 @@ function App() {
         name: taskName,
         text: taskText,
         done: false,
-        isExpanded: false,
+        isExpanded: true,
+        isEditing: false,
       },
     ]);
     setTaskText("");
@@ -107,6 +167,7 @@ function App() {
         task.id === id ? { ...task, isExpanded: !task.isExpanded } : task
       )
     );
+    console.log(tasks);
   };
 
   const handleCancelTask = () => {
